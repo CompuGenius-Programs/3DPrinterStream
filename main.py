823dab58c40c1b3bf79c5a9f923f55df0a54313d
CompuGenius <contact@cgprograms.com>
2024-11-14 23:03:14 -0500

    Added turning off smart outlet

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import time
 from dataclasses import asdict
@@ -5,6 +6,7 @@ from dataclasses import asdict
 import obsws_python as obs
 from bambu_connect import BambuClient, PrinterStatus
 from dotenv import load_dotenv
+from kasa import Discover
 
 load_dotenv()
 
@@ -14,6 +16,10 @@ serial = os.getenv('SERIAL')
 obs_host = os.getenv('OBS_HOST')
 obs_port = os.getenv('OBS_PORT')
 obs_password = os.getenv('OBS_PASSWORD')
+outlet_ip = os.getenv('OUTLET_IP')
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+turn_outlet_off = os.getenv('TURN_OUTLET_OFF')
 
 CHAMBER_LIGHT_OFF = '{"system": {"sequence_id": "0", "command": "ledctrl", "led_node": "chamber_light", "led_mode": "off", "led_on_time": 500, "led_off_time": 500, "loop_times": 0, "interval_time": 0}}'
 
@@ -34,13 +40,23 @@ def turn_off_light():
     bambu_client.executeClient.send_command(CHAMBER_LIGHT_OFF)
 
 
+async def turn_off_printer():
+    device = await Discover.discover_single(outlet_ip, username=username, password=password)
+    await device.update()
+    await device.turn_off()
+
+
 def custom_callback(msg: PrinterStatus):
     status = asdict(msg)
     if status['gcode_state'] == 'RUNNING' and not is_streaming():
         stream(True)
     elif status['gcode_state'] != 'RUNNING' and is_streaming():
         stream(False)
+        time.sleep(5)
         turn_off_light()
+        if turn_outlet_off == 'True':
+            time.sleep(2 * 60)
+            asyncio.run(turn_off_printer())
 
 
 def on_watch_client_connect():
